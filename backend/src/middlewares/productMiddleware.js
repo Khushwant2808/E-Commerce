e2774317f12138589ee1087324fd0e57cc6ff54c
Yash Agrawal <ayash@Yashs-MacBook-Air.local>
2026-01-24 16:48:07 +0530

    verify Seller + optimisation

---
+++ backend/src/middlewares/productMiddleware.js
@@ -0,0 +1,25 @@
+const { User } = require("../models");
+
+const verifyIfSeller = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findByPk(userId, {
+      attributes: ["canSell", "role"]  //OPTIMISATION
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    if (!user.canSell && user.role !== "admin") {
+      return res.status(403).json({ message: "Seller access required" });
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).json({ message: "Authorization failed" });
+  }
+};
+
+module.exports = { verifyIfSeller };
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { addProduct, getProductById, getProducts } = require("../controllers/productController");
+const { authenticateToken } = require("../middlewares/authMiddleware")
+const { verifyIfSeller } = require("../middlewares/productMiddleware")
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.post("/", addProduct);
+router.post("/", authenticateToken, verifyIfSeller, addProduct);
 
 module.exports = router;
