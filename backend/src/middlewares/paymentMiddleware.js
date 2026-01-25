c5c5fcdd7e4dc98517c7a9780c35aaab8007b44d
Yash Agrawal <ya828197@gmail.com>
2026-01-26 00:34:10 +0530

    payment middleware

---
+++ backend/src/middlewares/paymentMiddleware.js
@@ -0,0 +1,8 @@
+const { Payment } = require("../models")
+
+const verifyPayment = async (req, res, next) => {
+    console.log("Payment Verified ", req.user.id)
+    next();
+}
+
+module.exports = { verifyPayment }
